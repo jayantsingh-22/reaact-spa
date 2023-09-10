295a48fb2b59947cd081c8c903f35656d55c83d7
jayantsingh-22 <jayantsingh640@gmail.com>
2023-09-10 14:10:10 +0530

    added:changes

---
+++ src/pages/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+    </section>
+  );
+};
+
+export default Welcome;
---
+++ src/pages/WelcomeNewUser.js
@@ -0,0 +1,13 @@
+// WelcomeNewUser.js
+import React from "react";
+
+function WelcomeNewUser() {
+  return (
+    <section>
+      <h1>The Welcome Page</h1>
+      <p>Welcome, new user!</p>
+    </section>
+  );
+}
+
+export default WelcomeNewUser;
